c97416966f906c9715e20ec2e3843e1e37178f80
Dhamar Martínez <39349434+dhamarmj@users.noreply.github.com>
2022-03-11 10:24:13 -0500

    End of section 4

--- API/Controllers/BaseAPIController.cs
+++ API/Controllers/BaseAPIController.cs
@@ -1,11 +1,19 @@
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
-
+using Microsoft.Extensions.DependencyInjection;
 namespace API.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
     public class BaseAPIController : ControllerBase
     {
+        private IMediator _mediator;
+
+        //to use in all the children of this class
+        // ??= is a ternary conditioner that questions id its null 
+        // if _mediator is null -> send the value from the dependency
+        // if its not null send it normally
+        protected IMediator Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
 
     }
 }
